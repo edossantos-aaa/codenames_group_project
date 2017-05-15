8d628606e8a0aa1074ece5728c32d33d185b8736
Max Reichman and Peter Karellas <max@>
2017-05-15 11:32:29 -0700

    Create initial layout of UI.

---
+++ public/js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(".red-male").draggable();
+  $(".red-female").draggable();
+  $(".blue-male").draggable();
+  $(".blue-female").draggable();
+  $(".bystander-Male").draggable();
+  $(".assassin-Male").draggable();
+  $(".doubleagent-red").draggable();
+  $(".doubleagent-blue").draggable();
+});
